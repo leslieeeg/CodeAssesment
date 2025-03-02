4664cf6197f81fd53873d726b04ddfc5e2ad76c7
leslieeeg <lesliejunag08@gmail.com>
2025-03-02 12:49:24 -0500

    Level 2

--- main.py
+++ main.py
@@ -14,4 +14,20 @@ def list_free_tables(tables):
       free_tables.append(table["table_id"])
 
   return free_tables
+
+# Level 2
+"""
+Goal: Given a party size (e.g., 2, 3, 4 people), find one table that can seat at least that many people (i.e., capacity >= party_size) and is free.
+Output: Return a single table (or table ID) that fits the requirement.
+Results in a table ID that fits what user wants
+"""
+def one_for size(tables, psize):
+  for table in tables:
+    #see if not in use
+    if table["occupied"] == False and table["capacity"] >= party_size:
+      return table["table_id"] #conditions met then table ID returned
+
+return None
+
+
   
